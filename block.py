a1817f76abe1768dc1177e0b32e95f44a85f2bf1
gosha <nemirovsky.georgyi@gmail.com>
2021-11-23 18:07:50 +0300

    draft

--- block.py
+++ block.py
@@ -1,19 +1,19 @@
+from mobs import *
 import pygame
 from pygame import surface
-size_block = 40
 
 
 class Block():
     def __init__(self, x, y):
         self.x0 = x
         self.y0 = y
+        self.size_block = 40
 
     def draw(self, surface):
-        global size_block
-        self.surface = pygame.Surface((size_block, size_block))
+        self.surface = pygame.Surface((self.size_block, self.size_block))
         pygame.draw.rect(self.surface, (100, 100, 255),
-                         (0, 0, size_block, size_block))
-        surface.blit(self.surface, (self.x0 * size_block, self.y0 * size_block))
+                         (0, 0, self.size_block, self.size_block))
+        surface.blit(self.surface, (self.x0 * self.size_block, self.y0 * self.size_block))
 
 
 massive_block=[]
@@ -21,14 +21,25 @@ for i in range(5):
     for j in range(5):
         massive_block.append(Block(i, j))
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
-for i in massive_block:
-    i.draw(screen)
+screen = pygame.display.set_mode((400, 400), pygame.RESIZABLE)
+main_hero = main_person(100, 100, screen)
 finished = False
+FPS = 60
+clock = pygame.time.Clock()
 pygame.display.update()
 while not finished:
-    pygame.display.update()
+    screen.fill((0, 0, 0))
+    for i in massive_block:
+        i.draw(screen)
+    main_hero.draw()
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        main_hero.move(5)
+    elif keys[pygame.K_a]:
+        main_hero.move(-5)
+    pygame.display.update()
 pygame.quit()

78163909ceed177bb711aadf8c2be938e369665e
Mirzam2 <666mopsegor@gmail.com>
2021-11-23 20:49:51 +0300

    Проработка блоков

--- block.py
+++ block.py
@@ -10,22 +10,32 @@ class Block():
         self.size_block = 40
         self.surface = pygame.Surface((self.size_block, self.size_block))
         pygame.draw.rect(self.surface, (150, 150, 150),
-                         (0, 0, self.size_block, self.size_block),width=2) #картинку сюда
+                         (0, 0, self.size_block, self.size_block), width=1)  # обводка, потом можно убрать , так будет делать карту проще
 
     def draw(self, surface):
-        surface.blit(self.surface, (self.x0 * self.size_block, self.y0 * self.size_block))
-class Air_block(Block):
-    def __init__(self,x,y):
-        super().__init__(self,x,y)
-        self.permeability = True #проницаемость
-        self.durability = 0 #прочность
+        surface.blit(self.surface, (self.x0 * self.size_block,
+                     self.y0 * self.size_block))
+
+
+class Air_Block(Block):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.permeability = True  # проницаемость
+        self.durability = 0  # прочность
+        pygame.draw.rect(self.surface, (150, 150, 250),
+                         (0, 0, self.size_block, self.size_block))  # картинку сюда
+
 
 def generate_map(massive_block):
     for i in range(5):
-        for j in range(5):
+        for j in range(1,5):
             massive_block.append(Block(i, j))
+    for i in range(5):
+        massive_block.append(Air_Block(i, 0))
+
+
 if __name__ == "__main__":
-    massive_block =[]
+    massive_block = []
     generate_map(massive_block)
     pygame.init()
     screen = pygame.display.set_mode((400, 400))

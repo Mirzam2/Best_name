7fc5f33dc3be53f6a56e2e83322cd46e14f906b9
Mirzam2 <666mopsegor@gmail.com>
2021-11-26 09:42:06 +0300

    commit

---
+++ file.py
@@ -0,0 +1,27 @@
+import json
+import pickle
+import pathlib
+from pathlib import Path
+
+
+def save_map(massive_block: list, world_name="test"):
+    """
+    функция сохранения данных карты мира
+    massive_block - массив для блоков
+    world_name - название мира который нужно сохранять
+    """
+    data = []
+    for block in massive_block:
+        data.append([str(type(block)), block.x0, block.y0])
+    file = pathlib.Path(pathlib.Path.cwd(), "saves", world_name+".json")
+    with open(file, 'w') as f:
+        json.dump(data, f)
+
+
+def load_map(massive_block: list, world_name="test"):
+    """
+    функция загрузки данных карты мира
+    massive_block - массив для блоков
+    world_name - название мира который нужно загружать
+    """
+    file = pathlib.Path(pathlib.Path.cwd(), "saves", world_name+".json")
